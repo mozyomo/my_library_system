d4e59a7e882809b759ca8b93b705ae13120c0813
mozyomo <motoki619@gmail.com>
2024-01-20 23:05:29 +0900

    テストデータの挿入

--- db/migrate/20240117135749_create_stores.rb
+++
@@ -1,9 +0,0 @@
-class CreateStores < ActiveRecord::Migration[7.0]
-  def change
-    create_table :stores do |t|
-      t.string :store_name
-      t.integer :store_type
-      t.timestamps
-    end
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+5.times do |i|
+    User.create!(email: "motoki_#{i}@hmail.com", password: "password_#{i}" , created_at: Time.current, updated_at: Time.current)
+end
+
+User.all.each do |user|
+    2.times do |i|
+        user.stores.create!(store_name: "本棚_#{i}", store_type: i, created_at: Time.current, updated_at: Time.current)
+    end
+end
+
+Store.all.each do |store|
+    5.times do |i|
+        store.contents.create!(content_name: "本の題名_#{i}", content_type: 1, author_name: "著者名_#{i}", publisher_name: "出版社名_#{i}", published_at: Time.current, translator_name: "翻訳者名_#{i}", created_at: Time.current, updated_at: Time.current)
+    end
+end
